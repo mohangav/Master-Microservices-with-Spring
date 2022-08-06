a6d882bb84c86e1353b2f0b699421b3317aee238
mohangav <mohangavande@outlook.com>
2022-08-06 23:05:12 +0200

    delete test class

--- chapter 4/loans/src/test/java/com/eazybytes/loans/LoansApplicationTests.java
+++ chapter 4/loans/src/test/java/com/eazybytes/loans/LoansApplicationTests.java
@@ -3,7 +3,7 @@ package com.eazybytes.loans;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class LoansApplicationTests {
 
 	@Test
