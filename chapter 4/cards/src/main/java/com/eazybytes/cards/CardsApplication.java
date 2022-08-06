90257f92d88d4a5be7274eab7665baaccc663a3a
mohangav <mohangavande@outlook.com>
2022-08-07 00:43:25 +0200

    refresh scope

--- chapter 4/cards/src/main/java/com/eazybytes/cards/CardsApplication.java
+++ chapter 4/cards/src/main/java/com/eazybytes/cards/CardsApplication.java
@@ -3,6 +3,7 @@ package com.eazybytes.cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
 @EnableJpaRepositories("com.eazybytes.cards.repository")
 @EntityScan("com.eazybytes.cards.model")
+@RefreshScope
 public class CardsApplication {
 
 	public static void main(String[] args) {
