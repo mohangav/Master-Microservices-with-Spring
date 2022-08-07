cc0483d9ba801b8d7087a7e4e957812b4cada404
mohangav <mohangavande@outlook.com>
2022-08-07 19:38:28 +0200

    Added EurekaServer

--- chapter 4/accounts/src/main/java/com/eazytbytes/accounts/controller/AccountController.java
+++ chapter 4/accounts/src/main/java/com/eazytbytes/accounts/controller/AccountController.java
@@ -1,7 +1,9 @@
 package com.eazytbytes.accounts.controller;
 
 import com.eazytbytes.accounts.config.AccountsServiceConfig;
-import com.eazytbytes.accounts.model.Properties;
+import com.eazytbytes.accounts.model.*;
+import com.eazytbytes.accounts.service.client.CardsFeignClient;
+import com.eazytbytes.accounts.service.client.LoansFeignClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -11,9 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.eazytbytes.accounts.model.Accounts;
-import com.eazytbytes.accounts.model.Customer;
 import com.eazytbytes.accounts.repository.AccountsRepository;
+
+import java.util.List;
 
 @RestController
 public class AccountController {
@@ -23,6 +25,12 @@ public class AccountController {
 
 	@Autowired
 	AccountsServiceConfig accountsConfig;
+
+	@Autowired
+	LoansFeignClient loansFeignClient;
+
+	@Autowired
+	CardsFeignClient cardsFeignClient;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -42,6 +50,21 @@ public class AccountController {
 				accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
 		String jsonStr = ow.writeValueAsString(properties);
 		return jsonStr;
+	}
+
+	@PostMapping("/myCustomerDetails")
+	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+		List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(accounts);
+		customerDetails.setLoans(loans);
+		customerDetails.setCards(cards);
+
+		return customerDetails;
+
 	}
 
 }
---
+++ chapter 4/accounts/src/main/java/com/eazytbytes/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,17 @@
+package com.eazytbytes.accounts.service.client;
+
+import com.eazytbytes.accounts.model.Customer;
+import com.eazytbytes.accounts.model.Loans;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@FeignClient("loans")
+public interface LoansFeignClient {
+
+    @RequestMapping(method = RequestMethod.POST, value = "myLoans", consumes = "application/json")
+    List<Loans> getLoansDetails(@RequestBody Customer customer);
+}
